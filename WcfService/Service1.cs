21bdd6a7075130220ff9cd7b638b4de83e8a665c
DevinLevy <analescheok@hotmail.com>
2013-12-09 01:43:51 -0700

    commit2

---
+++ WcfService/Service1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Web.Security;
+
+namespace WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+        public bool CheckTheLogIn(string username, string password)
+        {
+            bool check = false;
+            SqlConnection sqlConnection = new SqlConnection("data source=.;initial catalog=ClassProjectDataBase;integrated security=True");
+            String SQLQuery = "SELECT * FROM people WHERE username = @userName and password = @password";
+            SqlCommand command = new SqlCommand(SQLQuery, sqlConnection);
+            command.Parameters.Add(new SqlParameter("@userName", username));
+            command.Parameters.Add(new SqlParameter("@password", password));
+            sqlConnection.Open();
+            DataTable person = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(command);
+            da.Fill(person);
+            da.Dispose();
+            sqlConnection.Close();
+            if (person.Rows.Count > 0)
+                check = true;
+            //if (Membership.ValidateUser(username, password))
+            //{
+            //    check = true;
+            //}
+
+
+            return check;
+        }
+
+
+    }
+}
---
+++ WcfServiceHost/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WcfService;
+
+namespace WcfServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Out.WriteLine("Starting Host");
+                ServiceHost host = new ServiceHost(typeof(WcfService.Service1));
+                host.Open();
+                Console.Out.WriteLine("Service started; enter Return to exit application");
+                Console.ReadLine();
+                host.Close();
+            }
+            catch (Exception e)
+            {
+                Console.Out.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
